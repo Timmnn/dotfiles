e8f84c19a9cd92e6c5fc01489aabd2cd11abdbd7
Timm Nicolaizik <timmmmnn@gmail.com>
2025-05-27 17:48:41 +0200

    Updated Packages

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,4 @@
-packages=("extra/bat", "extra/fzf", "aur/waypaper", "extra/swaybg", "extra/waybar", "extra/python-pywal", "extra/discord", "extra/shfmt", "aur/spotify", "extra/inotify-tools", "extra/pulsemixer", "extra/foot", "extra/sway")
+packages=("extra/bat", "extra/fzf", "aur/waypaper", "extra/swaybg", "extra/waybar", "extra/python-pywal", "extra/discord", "extra/shfmt", "aur/spotify", "extra/inotify-tools", "extra/pulsemixer", "extra/foot", "extra/sway", "extra/tldr", "extra/pavucontrol", "extra/bluez", "extra/bluez-utils", "extra/blueman", "extra/fish", "extra/noto-fonts-emoji")
 
 for package in "${packages[@]}"; do
   yay -S "$package"
